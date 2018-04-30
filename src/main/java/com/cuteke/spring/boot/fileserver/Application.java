5a4ff770e9f996d3cad51ab916fe944dacc3e64e
CuteKe <839713259@qq.com>
2018-04-30 18:53:03 +0800

    init

---
+++ src/main/java/com/cuteke/spring/boot/fileserver/Application.java
@@ -0,0 +1,29 @@
+package com.cuteke.spring.boot.fileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	/**
+	 * 文件上传配置  
+	 * @return
+	 */
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//文件最大  
+		factory.setMaxFileSize("10240KB"); //KB,MB  
+		/// 设置总上传数据总大小  
+		factory.setMaxRequestSize("10240KB");
+		return factory.createMultipartConfig();
+	}
+}
